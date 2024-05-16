6721b66f4ca835dd994a824184ccc86426155acc
Marcus Wagner <marcus.wagner@tum.de>
2024-05-16 11:55:45 +0900

    excluding _ca files for foldseek benchmark

--- src/benchmark_foldseek.sh
+++ src/benchmark_foldseek.sh
@@ -4,7 +4,7 @@
 date 
 
 ## foldseek easy-search (combination of all convert2db + prefilter + align)
-./lib/foldseek/build/src/foldseek easy-search ./out/dbs/foldseek_recoded/ ./data/dbs/subset_scope/ ./out/benchmark/foldseek/results ./out/benchmark/foldseek/tmp/ --threads 64 -s 9.5 --max-seqs 2000 -e 10
+./lib/foldseek/build/src/foldseek easy-search ./out/dbs/foldseek_recoded/ ./data/dbs/subset_scope_no_ca/ ./out/benchmark/foldseek/results ./out/benchmark/foldseek/tmp/ --threads 64 -s 9.5 --max-seqs 2000 -e 10
 
 ## end timing
 date
