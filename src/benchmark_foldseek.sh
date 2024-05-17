427fad1fc3512d700a9bc328af266a1a17a3ee35
Marcus Wagner <marcus.wagner@tum.de>
2024-05-17 17:38:58 +0900

    adapring foldseek search call for benchmark

--- src/benchmark_foldseek.sh
+++ src/benchmark_foldseek.sh
@@ -4,7 +4,7 @@
 date 
 
 ## foldseek easy-search (combination of all convert2db + prefilter + align)
-./lib/foldseek/build/src/foldseek easy-search ./out/dbs/foldseek_recoded/ ./data/dbs/subset_scope_no_ca/ ./out/benchmark/foldseek/results ./out/benchmark/foldseek/tmp/ --threads 64 -s 9.5 --max-seqs 2000 -e 10
+./lib/foldseek/build/src/foldseek search ./out/dbs/foldseek_recoded/subset_scope_recoded ./data/dbs/subset_scope_no_ca/subset_scope ./out/benchmark/foldseek/results/benchmark ./out/benchmark/foldseek/tmp/ --threads 64 -s 9.5 --max-seqs 2000 -e 10
 
 ## end timing
 date
